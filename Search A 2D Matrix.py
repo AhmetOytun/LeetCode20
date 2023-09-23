7dace27395a61b26c90a87001394192f199375c3
AhmetOytun <ahmetoytunkurtuldu@gmail.com>
2023-09-23 08:29:27 +0300

    2D Matrix Solution STILL Needs To Be Optimized

--- Search A 2D Matrix.py
+++ Search A 2D Matrix.py
@@ -5,29 +5,38 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         l1=l2=m1=m2=0
         r1,r2=len(matrix)-1,len(matrix[0])-1
+        print(f"r1={r1}")
+        print(f"l1={l1}")
         while r1>=l1:
+            print(f"r1={r1}")
+            print(f"l1={l1}")
             m1=(r1+l1)//2
-            print(f"m1={m1}")
-            print(len(matrix))
             if matrix[m1][0] <= target <= matrix[m1][len(matrix[0]) - 1]:
                 while r2 >= l2:
-                    print(f"m2={m2}")
                     m2 = (r2 + l2) // 2
-                    if matrix[m1][m2]>target:
+                    if matrix[m1][m2]!=target and len(matrix[m1])==1:
+                        return False
+                    elif matrix[m1][m2]==target and len(matrix[m1])==1:
+                        return True
+                    elif matrix[m1][m2]>target:
                         r2=m2-1
                     elif matrix[m1][m2]<target:
                         l2=m2+1
                     else:
                         return True
                 return False
-            elif matrix[m1][0]>target:
-                r1=m1-1
+            elif matrix[m1][0]!=target and len(matrix)==1:
+                return False
+            elif matrix[m1][0]==target and len(matrix)==1:
+                return True
             elif matrix[m1][0]<target:
+                r1=m1-1
+            elif matrix[m1][0]>target:
                 l1=m1-1
             else:
                 return False
         return False
-print(Solution.searchMatrix(Solution,[[1,3,5,7],[10,11,16,20],[23,30,34,60]],11))
+print(Solution.searchMatrix(Solution,[[1,3,5,7],[10,11,16,20],[23,30,34,60]],3))
 
 
 
