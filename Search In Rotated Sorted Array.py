54f9eb2d7afd60a6eeecd9b7fcd9c14a875f3cab
AhmetOytun <ahmetoytunkurtuldu@gmail.com>
2023-09-27 08:20:30 +0300

    added solution search in rotated sorted array revision this problem later(you looked it up)

---
+++ Search In Rotated Sorted Array.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l,r=0,len(nums)-1
+        while r>=l:
+            m=(r+l)//2
+            if target == nums[m]:
+                return nums[m]
+            if nums[l]<=nums[m]:
+                if target>nums[m] or target<nums[l]:
+                    l=m+1
+                else:
+                    r=m-1
+            else:
+                if target<nums[m] or target>nums[r]:
+                    r=m-1
+                else:
+                    l=m+1
+        return -1
