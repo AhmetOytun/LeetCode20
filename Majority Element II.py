643bb9a1da243c20c68c4dcba081a5073bb80abe
AhmetOytun <ahmetoytunkurtuldu@gmail.com>
2023-10-05 08:05:28 +0300

    optimized even further

--- Majority Element II.py
+++ Majority Element II.py
@@ -4,15 +4,13 @@ from typing import List
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         length=len(nums)/3
-        dic={}
+        nset=set()
         sol=[]
         for x in nums:
-            if x not in dic:
-                dic[x]=nums.count(x)
-                if dic[x]>length:
+            if x not in nset:
+                nset.add(x)
+                if nums.count(x)>length:
                     sol.append(x)
-            else:
-                continue
         return sol
 print(Solution.majorityElement(Solution,[3,2,3]))
 
